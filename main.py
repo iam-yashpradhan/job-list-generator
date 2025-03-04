a9cc4f000195f119e7f7283ae72043167c723b40
Yash Pradhan <iamyashpradhan@gmail.com>
2025-03-04 09:24:30 -0500

    added instructions and api key from user

--- main.py
+++ main.py
@@ -7,10 +7,15 @@ import dotenv
 
 dotenv.load_dotenv()
 
-api_key = os.getenv("TAVILY_API_KEY")
-client = TavilyClient(api_key)
-
 st.sidebar.header("API Key Input")
+st.sidebar.markdown("""
+To get your Tavily API key:
+1. Go to [Tavily](https://tavily.com/)
+2. Create your free account
+3. You will get 1000 API requests per month
+4. One request is one click of the generate button
+5. Even if you use 20 requests per day, you will still be left with 400 more requests
+""")
 api_key = st.sidebar.text_input("Enter your Tavily API Key")
 
 if api_key:
@@ -30,7 +35,8 @@ def tavily_run(time_range, prompt):
     max_results=20,
     time_range=time_range,
     include_answer="advanced",
-    include_domains=["dice.com", "linkedin.com", "simplyhired.com"],
+    include_domains=["dice.com"],
+   
 )
     return response
 
@@ -38,6 +44,7 @@ if generate_btn:
     prompt = f"I want you to extract job postings for the role of {job_role} from the provided domains in United States of America. Do not include blog posts"
     response = tavily_run(time_range, prompt)
     results = response['results']
+    st.write(results)
     data = [{'title': result['title'], 'url': result['url']} for result in results]
     
     job_list = pd.DataFrame(data)
